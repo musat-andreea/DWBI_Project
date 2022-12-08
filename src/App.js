4c33195bcc3f1e7f700464750e4c96768cff67c4
Andreea <andree98Black@yahoo.com>
2022-12-08 22:51:11 +0200

    Done Landing Page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Feature from "./Components/Feature";
 import About from "./Components/About";
 import aboutimage from "./images/about.jpg"
 import aboutimage1 from "./images/about2.jpg";
+import Presentation from "./Components/Presentation";
+import Contact from "./Components/Contact";
 
 function App() {
     const title = 'Welcome to your health journal!';
@@ -16,8 +18,10 @@ function App() {
       <div className='App'>
           <Header/>
           <Feature/>
+          <Presentation/>
           <About image={aboutimage} title='Comes With All You Need For Better Life' button='Make an appointment' />
           <About image={aboutimage1} title='Meet a new doctor' button='Schedule a call' />
+          <Contact/>
       </div>
       // <div style={{
       //     backgroundImage: `url("https://thumbs.dreamstime.com/b/healthcare-technology-doctor-using-digital-tablet-icon-medical-network-hospital-background-162019727.jpg")`
